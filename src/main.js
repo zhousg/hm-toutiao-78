8334230bc18b2e8a8e711143676e4ede8fd0e954
zhousg <zhoushugang@itcast.cn>
2019-08-01 16:22:47 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 产品级别提示  不开启  开发阶段 提示越详细越好  开发级别提示
 Vue.config.productionTip = false
 
+// main.js 入口文件
+// 职责1：引入需要依赖的模块（js,包，css , 其他资源）
+// 职责2：创建一个根vue实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
