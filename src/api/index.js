4cd72237307b3014483012f206697a2dd7346a46
zhousg <zhoushugang@itcast.cn>
2019-08-04 18:48:39 +0800

    async与await使用

--- src/api/index.js
+++ src/api/index.js
@@ -6,8 +6,33 @@ import store from '@/store'
 // 基准路径
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 // 请求头字段
-axios.defaults.headers = {
-  Authorization: `Bearer ${store.getUser().token}`
-}
+// axios.defaults.headers = {
+//   Authorization: `Bearer ${store.getUser().token}`
+// }
+
+axios.interceptors.request.use((config) => {
+  // 修改  在每次请求前 获取一次token 设置头部
+  // 好处：不会覆盖之前的头部信息
+  config.headers.Authorization = `Bearer ${store.getUser().token}`
+  return config
+}, (err) => {
+  return Promise.reject(err)
+})
+
+axios.interceptors.response.use((res) => {
+  // 成功的返回值 默认包装成promise对象
+  return res
+}, (err) => {
+  // 获取响应状态码
+  // err 错误对象
+  // err.response 响应对象
+  // err.response.status 响应状态码
+  if (err.response.status === 401) {
+    // token失效  跳转登录页面
+    // this.$router.push('/login')  本质  把hash改成了 #/login
+    location.hash = '#/login'
+  }
+  return Promise.reject(err)
+})
 
 export default axios
