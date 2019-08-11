92921ab542b3a8875d132275fe951057bf9e7fa9
zhousg <zhoushugang@itcast.cn>
2019-08-11 15:23:47 +0800

    评论管理实现

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Article from '@/views/article'
 import NotFound from '@/views/404'
 import Image from '@/views/image'
 import Publish from '@/views/publish'
+import Comment from '@/views/comment'
 
 import store from '@/store'
 
@@ -34,7 +35,8 @@ const router = new VueRouter({
         { path: '/', name: 'welcome', component: Welcome },
         { path: '/article', name: 'article', component: Article },
         { path: '/image', name: 'image', component: Image },
-        { path: '/publish', name: 'publish', component: Publish }
+        { path: '/publish', name: 'publish', component: Publish },
+        { path: '/comment', name: 'comment', component: Comment }
       ]
     },
     // 路径 404
